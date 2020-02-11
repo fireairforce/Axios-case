a85a27c382f50e993422bcbcb3ae97de7b9f74a9
fireairforce <1344492820@qq.com>
2020-02-11 21:50:17 +0800

    feat:新增error信息的测试页面

---
+++ examples/error/app.ts
@@ -0,0 +1,52 @@
+import axios from '../../src/index'
+
+// 故意写错url
+axios({
+  method: 'get',
+  url: '/error/get1'
+})
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e)
+  })
+
+axios({
+  method: 'get',
+  url: '/error/get'
+})
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e)
+  })
+
+setTimeout(() => {
+  axios({
+    method: 'get',
+    url: '/error/get'
+  })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+}, 5000)
+
+axios({
+  method: 'get',
+  url: '/error/get'
+})
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e.message)
+    console.log(e.config)
+    console.log(e.code)
+    console.log(e.request)
+    console.log(e.isAxiosError)
+  })
