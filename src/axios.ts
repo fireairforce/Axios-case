aa27049f170bc233011ed4a1ea3aee75d465aec5
fireairforce <1344492820@qq.com>
2020-02-12 21:46:00 +0800

    feat:axios混合对象请求的demo样例编写

--- src/axios.ts
+++ src/axios.ts
@@ -4,9 +4,12 @@ import { extend } from './helpers/util'
 
 function createInstance(): AxiosInstance {
   const context = new Axios()
+  // 获取到Axios原型上request方对象
   const instance = Axios.prototype.request.bind(context)
 
+  // 把实例都转移到instance对象上面去
   extend(instance, context)
+  // 类型断言一下
   return instance as AxiosInstance
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axios from '../src/core/Axios'
+import axios from './axios'
 
 // 把types里面的类型全部导出出去给外部的变量使用
 export * from './types'
