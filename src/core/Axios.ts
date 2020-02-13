c748465bccccbd27f6cc15ac6d23dc2f3d8ce21f
fireairforce <1344492820@qq.com>
2020-02-13 20:24:27 +0800

    feat:实现axios拦截器的链式调用

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,7 +1,34 @@
-import { AxiosRequestConfig, AxiosPromise, Method } from '../types'
+import {
+  AxiosRequestConfig,
+  AxiosPromise,
+  Method,
+  AxiosResponse,
+  ResolvedFn,
+  RejectedFn
+} from '../types'
 import dispatchRequest from './dispatchRequest'
+import InterceptorManager from './interceptorManager'
+
+interface Interceptors {
+  request: InterceptorManager<AxiosRequestConfig>
+  response: InterceptorManager<AxiosResponse>
+}
+
+interface PromiseChain<T> {
+  resolved: ResolvedFn<T> | ((config: AxiosRequestConfig) => AxiosPromise)
+  rejected?: RejectedFn
+}
 
 export default class Axios {
+  interceptors: Interceptors
+
+  // 拦截器
+  constructor() {
+    this.interceptors = {
+      request: new InterceptorManager<AxiosRequestConfig>(),
+      response: new InterceptorManager<AxiosResponse>()
+    }
+  }
   //   实现request请求
   request(url: any, config?: any): AxiosPromise {
     if (typeof url === 'string') {
@@ -12,6 +39,29 @@ export default class Axios {
     } else {
       config = url
     }
+    // 实现拦截器的链式调用
+    const chain: PromiseChain<any>[] = [
+      {
+        resolved: dispatchRequest,
+        rejected: undefined
+      }
+    ]
+    //  对于请求的拦截器是后添加的先执行
+    this.interceptors.request.forEach(interceptor => {
+      chain.unshift(interceptor)
+    })
+
+    // 响应拦截器则是先添加的先执行
+    this.interceptors.request.forEach(interceptor => {
+      chain.push(interceptor)
+    })
+
+    let promise = Promise.resolve(config)
+    while (chain.length) {
+      const { resolved, rejected } = chain.shift()!
+      promise.then(resolved, rejected)
+    }
+
     return dispatchRequest(config)
   }
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
